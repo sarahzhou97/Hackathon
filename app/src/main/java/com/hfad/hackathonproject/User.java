f1e23dc29074abc1fb0dcd3859a9df8ae3e8d1c6
sarezhou <sarezhou@gmail.com>
2016-07-28 23:43:17 -0700

    added login page

---
+++ app/src/main/java/com/hfad/hackathonproject/User.java
@@ -0,0 +1,14 @@
+package com.hfad.hackathonproject;
+
+/**
+ * Created by sarahzhou on 7/28/16.
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.password=password;
+        this.username=username;
+    }
+}
