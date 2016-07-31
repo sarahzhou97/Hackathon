9daa4256665c4df42e0900e46f55bf54f77a00d1
sarezhou <sarezhou@gmail.com>
2016-07-31 01:06:24 -0700

    took out action bars

--- app/src/main/java/com/hfad/hackathonproject/RestaurantDescription.java
+++ app/src/main/java/com/hfad/hackathonproject/RestaurantDescription.java
@@ -1,32 +1,49 @@
 package com.hfad.hackathonproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class RestaurantDescription extends AppCompatActivity {
+
+    AzureConnector connector = new AzureConnector();
+   ArrayList<Restaurant> restaurants = connector.getRestaurants("RESTAURANTS");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_description);
+        Intent intent = getIntent();
 
         ImageView icon = (ImageView)findViewById(R.id.RestaurantIcon);
         TextView categories = (TextView)findViewById(R.id.categories);
         TextView website = (TextView)findViewById(R.id.website);
-        
-        website.setText();
+
+
+
 
     }
 
-    public void gotowebsite() {
-
+    public void gotowebsite(View view) {
+        goToUrl()
     }
 
     public void gotomaps() {
 
+    }
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW,uriUrl);
+        startActivity(launchBrowser);
     }
 }
