c78b431f5f88ff2f41c850f184d30ec876b797e3
sarezhou <sarezhou@gmail.com>
2016-07-30 17:15:30 -0700

    Created Restaurant Description page

---
+++ app/src/main/java/com/hfad/hackathonproject/RestaurantDescription.java
@@ -0,0 +1,21 @@
+package com.hfad.hackathonproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant_description);
+    }
+
+    public void gotowebsite() {
+
+    }
+
+    public void gotomaps() {
+
+    }
+}
